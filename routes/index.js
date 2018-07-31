37d4ae2b3b4c3e1c3a9e002aff0b89bba47822ad
MANISH <mk6598951@gmail.com>
2018-08-01 01:30:47 +0530

    making template

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', {home:'home' });
+});
+
+router.get('/about',(req,res,next)=>{
+  res.render('about',{about:'about'});
+})
+
+router.get('/courses',(req,res)=>{
+  res.render('courses',{courses:'courses'});
+})
+
+router.get('/contact',(req,res)=>{
+  res.render('contact',{contact:'contact'});
+})
+
+router.get('/services',(req,res)=>{
+  res.render('services',{services:'services'});
+})
+
+router.get('/join',(req,res)=>{
+  res.render('join',{});
+})
+
+router.get('/app',(req,res)=>{
+  res.render('app',{});
+})
+
+router.get('/404',(req,res)=>{
+  res.render('404',{});
+})
+
+module.exports = router;
