314c5dbb01c8c02793e92afad88951dcb03db366
JAIMAKALI <mk6598951@gmail.com>
2018-08-12 00:09:37 +0530

    dd

--- routes/what-we-do.js
+++ routes/what-we-do.js
@@ -5,5 +5,9 @@ router.get('/res-web-design',(req,res)=>{
     res.render('what_we_do/res_web_design');
 })
 
+router.get('/mobile-app-development',(req,res)=>{
+    res.render('what_we_do/mobile_development');
+})
+
 
 module.exports = router;
